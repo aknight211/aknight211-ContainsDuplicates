87ab48bb69761e644546c64513cc10428d865b65
Anthony Knight <aknight0211@gmail.com>
2025-06-22 03:43:11 +0000

    I created a new HashSet that will hold all the values in the array and I used .contains to see if any if the values are a duplicate

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,13 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Set<Integer> seen = new HashSet<>();
+        for(int num:nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            seen.add(num);
+        }
         return false;
     }
 }
